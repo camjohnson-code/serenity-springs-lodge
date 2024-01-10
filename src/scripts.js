38ea013b8d08f97d399850835df02e9741a8d714
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-01-10 16:01:32 -0700

    feature: Add function for toggling dropdown arrow

--- src/scripts.js
+++ src/scripts.js
@@ -5,9 +5,27 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import './images/logo.svg'
-import './images/login.jpg'
+import './images/turing-logo.png';
+import './images/logo.svg';
+import './images/login.jpg';
+import './images/home.svg';
+import './images/hotel.png';
+import './images/spending.png';
+import './images/calendar.png';
+import './images/github-mark-white.png';
+import './images/In-White-96.png';
+import './images/arrow.png';
+import './images/single.jpg';
+import './images/welcome-section.jpg';
 
+// console.log('This is the JavaScript entry file - your code begins here.');
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdownButtons = document.querySelectorAll('.nav-button');
+  const dropdownOptions = document.querySelector('.dropdown-options');
+  const toggleArrow = document.querySelector('.toggle-arrow');
 
-console.log('This is the JavaScript entry file - your code begins here.');
+  dropdownButtons[1].addEventListener('click', function () {
+    dropdownOptions.classList.toggle('active');
+    toggleArrow.classList.toggle('rotate');
+  });
+});
