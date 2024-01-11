16fede6443dc505a80c9bddcae0814c38e9c2e55
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-01-10 19:24:01 -0700

    refactor: Update js for dropdown toggle

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,6 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
 import './images/logo.svg';
 import './images/login.jpg';
 import './images/home.svg';
@@ -18,14 +17,41 @@ import './images/arrow.png';
 import './images/single.jpg';
 import './images/welcome-section.jpg';
 
-// console.log('This is the JavaScript entry file - your code begins here.');
-document.addEventListener('DOMContentLoaded', function () {
-  const dropdownButtons = document.querySelectorAll('.nav-button');
-  const dropdownOptions = document.querySelector('.dropdown-options');
-  const toggleArrow = document.querySelector('.toggle-arrow');
+// Global Variables
+const nav = document.querySelector('.customer-nav');
+const navButtons = document.querySelectorAll('.nav-button');
+const dropdownButtons = document.querySelectorAll('.dropdown-button');
+const dropdownOptions = document.querySelector('.dropdown-options');
+const toggleArrow = document.querySelector('.toggle-arrow');
 
-  dropdownButtons[1].addEventListener('click', function () {
+nav.addEventListener('click', function (event) {
+  if (
+    event.target.classList.contains('nav-button') ||
+    event.target.closest('button').classList.contains('nav-button')
+  ) {
+    navButtons.forEach((li) => li.classList.remove('active'));
+    event.target.classList.toggle('active');
+  }
+
+  // dropdown opening/closing only
+  if (
+    (event.target === navButtons[1] ||
+      event.target.closest('button') === navButtons[1]) &&
+    !event.target.classList.contains('dropdown-button')
+  ) {
     dropdownOptions.classList.toggle('active');
     toggleArrow.classList.toggle('rotate');
-  });
+  } else if (event.target.classList.contains('dropdown-button')) {
+    dropdownOptions.classList.add('active');
+    toggleArrow.classList.add('rotate');
+  } else {
+    console.log(event.target);
+    dropdownOptions.classList.remove('active');
+    toggleArrow.classList.remove('rotate');
+  }
+
+  if (event.target.classList.contains('dropdown-button')) {
+    dropdownButtons.forEach((button) => button.classList.remove('active'));
+    event.target.classList.add('active');
+  }
 });
